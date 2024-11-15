d9e67d49749ad3b4a1242a123904f658093c2043
DRAGGAME <maxutnov@mail.ru>
2024-11-16 01:01:52 +0300

    Upgrade

--- main.py
+++ main.py
@@ -56,27 +56,6 @@ async def vibor(message: types.Message):
         reply_markup=builder.as_markup())
     await bot.pin_chat_message(chat_id=message.chat.id, message_id=sent_message.message_id)
 
-
-@dp.callback_query(F.data == 'Geschiht')
-async def gechichte(callback: types.CallbackQuery):
-    await callback.message.answer(f'Выберите презентацию:\n'
-                                  f'1. Просвещение\n2. Политическая карта Европы '
-                                f'и мира в XVIII веке.pptx.Просвещение\n3. Новые идейно-политические течения и'
-                                f' традиции в XVIII веке\n4. Материальный и духовный мир человека XVIII века.pptx\n'
-                                f'5. Англия в XVIII веке.pptx.Просвещение\n6. Северная Америка в XVII веке\n'
-                                f'7. Начало конфликта между Англией и её североамериканскими колониями\n'
-                                f'8. Германские земли\n9. Презентация по 9 параграфу Демкин Алексей\n'
-                                f'10. Австрийская монархия Габсбругов в XVIII веке\n'
-                                f'11. Французская революция\n'
-                                f'Выберете какую надо вам презентацию')
-
-    builder = ReplyKeyboardBuilder()
-    for i in range(1, 12):
-        builder.add(types.KeyboardButton(text=str(i)))
-    builder.adjust(3)
-    await callback.answer()
-
-
 @dp.message(Command('help'))
 async def helpcmd(message: types.Message):
     await message.reply('Команды:\n'
@@ -86,59 +65,62 @@ async def helpcmd(message: types.Message):
                         'выведена презентация и удалена через 20 минут.\n\n'
                         'Если вы хотите вывести другие презентации нажмите на закреплённое сообщение.')
 
+@dp.callback_query(F.data == 'Geschiht')
+async def gechichte(callback: types.CallbackQuery):
+    builder = ReplyKeyboardBuilder()
+    status = F.data
+    for i in range(1, 13):
+        builder.add(types.KeyboardButton(text=str(i)))
+    builder.adjust(3)
+    await callback.message.answer(
+        f'Выберите презентацию:\n'
+        f'1. Просвещение\n2. Политическая карта Европы '
+        f'и мира в XVIII веке.pptx.Просвещение\n3. Новые идейно-политические течения и'
+        f' традиции в XVIII веке\n4. Материальный и духовный мир человека XVIII века.pptx\n'
+        f'5. Англия в XVIII веке.pptx.Просвещение\n6. Северная Америка в XVII веке\n'
+        f'7. Начало конфликта между Англией и её североамериканскими колониями\n'
+        f'8. Германские земли\n9. Презентация по 9 параграфу Демкин Алексей\n'
+        f'10. Австрийская монархия Габсбругов в XVIII веке\n'
+        f'11. Французская революция\n'
+        f'12. Индия\n'
+        f'Выберете какую надо вам презентацию',
+        reply_markup=builder.as_markup(resize_keyboard=True))
+    inputs('Презентации/1.Просвещение.pptx', '1')
+    inputs('Презентации/2. Политическая карта Европы и мира в XVIII веке.pptx', '2')
+    inputs('Презентации/3. Новые идейно-политические течения и традиции в XVIII веке.pptx', '3')
+    inputs('Презентации/4. Материальный и духовный мир человека XVIII века.pptx', '4')
+    inputs('Презентации/5. Англия в XVIII веке.pptx', '5')
+    inputs('Презентации/6. Северная Америка в XVII веке.pptx', '6')
+    inputs('Презентации/7. Начало конфликта между Англией и её североамериканскими колониями.pptx', '7')
+    inputs('Презентации/ГЕРМАНСКИЕ ЗЕМЛИ.pptx', '8')
+    inputs('Презентации/Презентация по 9 параграфу Демкин Алексей.pptx', '9')
+    inputs('Презентации/Австрийская монархия Габсбургов в XVIII веке.pptx', '10')
+    inputs('Презентации/французская революция2.pdf', '11')
+    inputs('Презентации/10. Индия.pptx', '12')
 
-inputs('Презентации/1.Просвещение.pptx', '1')
-inputs('Презентации/2. Политическая карта Европы и мира в XVIII веке.pptx', '2')
-inputs('Презентации/3. Новые идейно-политические течения и традиции в XVIII веке.pptx', '3')
-inputs('Презентации/4. Материальный и духовный мир человека XVIII века.pptx', '4')
-inputs('Презентации/5. Англия в XVIII веке.pptx', '5')
-inputs('Презентации/6. Северная Америка в XVII веке.pptx', '6')
-inputs('Презентации/7. Начало конфликта между Англией и её североамериканскими колониями.pptx', '7')
-inputs('Презентации/ГЕРМАНСКИЕ ЗЕМЛИ.pptx', '8')
-inputs('Презентации/Презентация по 9 параграфу Демкин Алексей.pptx', '9')
-inputs('Презентации/Австрийская монархия Габсбургов в XVIII веке.pptx', '10')
-inputs('Презентации/французская революция2.pdf', '11')
+    await callback.answer()
 
 
 
-
-
-
-
-
-
-
-
-# @dp.callback_query(F.data == "random_value")
-# async def send_random_value(callback: types.CallbackQuery):
-#     await callback.message.answer(str(randint(1, 10)))
-#     await callback.answer()
-#
-
-
-
-# star('1', 'Презентации/1.Просвещение.pptx')
-#
-# star('2', 'Презентации/2. Политическая карта Европы и мира в XVIII веке.pptx')
-#
-# star('3', 'Презентации/3. Новые идейно-политические течения и традиции в XVIII веке.pptx')
-#
-# star('4', 'Презентации/4. Материальный и духовный мир человека XVIII века.pptx')
-#
-# star('5', 'Презентации/5. Англия в XVIII веке.pptx')
-#
-# star('германские земли', 'Презентации/ГЕРМАНСКИЕ ЗЕМЛИ.pptx')
-#
-# star('франция(до революции)', 'Презентации/Презентация по 9 параграфу Демкин Алексей.pptx')
-#
-# star('австрия', 'Презентации/Австрийская монархия Габсбургов в XVIII веке.pptx')
-#
-# star('9', 'Презентации/6. Северная Америка в XVII веке.pptx')
-#
-# star('10', 'Презентации/7. Начало конфликта между Англией и её североамериканскими колониями.pptx')
-#
-# star('11', 'Презентации/французская революция2.pdf')
-
+@dp.callback_query(F.data == 'Obchaga')
+async def gechichte(callback: types.CallbackQuery):
+    builder = ReplyKeyboardBuilder()
+    for i in range(1, 4):
+        if i == 2:
+            builder.add(types.KeyboardButton(text='НЕТ'))
+        else:
+            builder.add(types.KeyboardButton(text=str(i)))
+    builder.adjust(2)
+    await callback.message.answer(
+        f'Выберите презентацию:\n'
+        f'1. Что делает человека человеком\n '
+        f'2. В процессе\n'
+        f'3. Общество как форма жизнедеятельности людей\n'
+        f'4. В процессе',
+        reply_markup=builder.as_markup(resize_keyboard=True))
+    inputs('Презентации/1. Что делает человека человеком.pptx', '1')
+    inputs('Презентации/3. Общество как форма жизнедеятельности людей.pptx', '3')
+    await callback.answer()
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
